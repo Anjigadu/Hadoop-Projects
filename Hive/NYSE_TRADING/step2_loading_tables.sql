af15f53009f549e378b58d7a48f2ad9e64439339
yuanhsin8311 <yuanhsin8311@gmail.com>
2017-01-13 15:25:50 -0800

    Update and rename loading_tables.sql to step2_loading_tables.sql

--- Hive/NYSE_TRADING/step2_loading_tables.sql
+++ Hive/NYSE_TRADING/step2_loading_tables.sql
@@ -1,8 +1,9 @@
 use handson_nasdaq;
 
---load data into the managed table tbl_nasdaq_daily_prices
+--load data into the managed table mng_daily_prices
 load data inpath '' overwrite into tbl_nasdaq_daily_prices;
-
+load data inpath '' 
+overwrite into mng_daily_prices;
 --load data into the managed parquet table tbl_nasdaq_daily_prices 
 insert overwrite table tbl_nasdaq_daily_prices_parquet
 select * from tbl_nasdaq_daily_prices;
