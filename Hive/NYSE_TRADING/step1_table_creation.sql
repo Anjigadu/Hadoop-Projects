a2d7b68394cecc0f73f0e6a3b2e22ae3613a456a
yuanhsin8311 <yuanhsin8311@gmail.com>
2017-01-14 23:17:53 -0800

    Update step1_table_creation.sql

--- Hive/NYSE_TRADING/step1_table_creation.sql
+++ Hive/NYSE_TRADING/step1_table_creation.sql
@@ -90,20 +90,31 @@ select symbol, sum(volume) total_stock_volume from mng_daily_prices group by sym
 
 hive -f stock_vol.hql
 
-
 -- Create an external table for NASDAQ dividends data set.
-create external table nasdaq_dividends (
-	exchange_name string,stock_symbol string, date string, dividends float
+create external table ext_daily_prices_pq (
+	exchange_name string,stock_symbol string, date string, price_open float, price_high float, price_low float,
+	price_close float, volume int, price_adj_close float
 )
-row format delimited
-fields terminated by ','
-location '/user/cloudera/rawdata/hadoop_class/nasdaq_dividend';
+stored as parquet
+location '/user/yuanhsin/rawdata/parquet_result/daily_prices';
 
--- create a managed avro table
-create table nasdaq_dividends_avro (
-	exchange_name string,stock_symbol string, date string, dividends float
-)
-stored as avro;
+insert overwrite table ext_daily_prices_pq 
+select * from ext_daily_prices where exchange_name != 'exchange';
+
+create table ext_daily_prices_avro
+stored as avro
+as
+select * from ext_daily_prices where exchange_name != 'exchange';
+
+hdfs dfs -ls -R .
+hdfs dfs -ls -R /user/yuanhsin/
+
+-- To recreate the sql statement used to create a table
+-- Also use the output to view the real details for building a hive table
+show create table ext_daily_prices;
+
+-- view details of avro table
+show create table ext_daily_prices_avro;  
 
 --alter table to add the schema location
 alter table nasdaq_dividends_avro
@@ -115,9 +126,4 @@ alter table nasdaq_dividends_avro set tblproperties('author'='michael enudi')
 -- alter table to rename the table
 alter table nasdaq_dividends_avro rename to avro_dividend	
 
--- to recreate the sql statement used to create a table
--- also use the output to view the real details for building a hive table
-show create table nasdaq_daily_prices;
 
--- view details of our new avro table
-show create table nasdaq_dividends_avro;
