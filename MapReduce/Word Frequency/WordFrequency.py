7b677ff2f3ee43234ebfb1b039e048febf359574
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-27 13:44:48 -0800

    Update WordFrequency.py

--- MapReduce/Word Frequency/WordFrequency.py
+++ MapReduce/Word Frequency/WordFrequency.py
@@ -1,17 +1,19 @@
-from mrjob.job import MRJob
+# Word Frequency Counter
 
+from mrjob.job import MRJob
 
 class MRWordFrequencyCount(MRJob):
 
     def mapper(self, _, line):
         words = line.split()
         for word in words:
-            word = unicode(word, "utf-8", errors="ignore") #avoids issues in mrjob 5.0
+            word = unicode(word, "utf-8", errors="ignore")     #avoids issues in mrjob 5.0
             yield word.lower(), 1
 
-    def reducer(self, key, values):
-        yield key, sum(values)
-
+    def reducer(self, key, value):
+        yield key, sum(value)
 
 if __name__ == '__main__':
     MRWordFrequencyCount.run()
+
+    
