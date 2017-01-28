471b636980a4a43252585937bf50a02e49cb710a
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-27 22:16:44 -0800

    Update spend-by-customer.py

--- MapReduce/Customer Order/spend-by-customer.py
+++ MapReduce/Customer Order/spend-by-customer.py
@@ -1,3 +1,5 @@
+# How much order amounts did each customer spend?
+# Column: CUSTOMER, ITEM, ORDER AMOUNT
 from mrjob.job import MRJob
 
 class SpendByCustomer(MRJob):
@@ -6,10 +8,22 @@ class SpendByCustomer(MRJob):
         (customerID, itemID, orderAmount) = line.split(',')
         yield customerID, float(orderAmount)
 
-    def reducer(self, customerID, orders):
-        yield customerID, sum(orders)
+    def reducer(self, customerID, orderAmount):
+        yield customerID, sum(orderAmount)
 
 
 if __name__ == '__main__':
     SpendByCustomer.run()
     
+# Output
+"0"	5524.950000000002
+"1"	4958.600000000001
+"10"	4819.7
+"11"	5152.29
+"12"	4664.589999999999
+"13"	4367.619999999999
+"14"	4735.030000000001
+"15"	5413.510000000001
+"16"	4979.060000000001
+"17"	5032.680000000001
+"18"	4921.27
