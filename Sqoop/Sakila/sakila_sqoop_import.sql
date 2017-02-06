760ab47fbcc06ced3394bfe0432cff8ecf41e4c5
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-05 23:30:24 -0800

    Update sakila_sqoop_import.sql

--- Sqoop/Sakila/sakila_sqoop_import.sql
+++ Sqoop/Sakila/sakila_sqoop_import.sql
@@ -7,7 +7,10 @@ show schemas;
 CREATE VIEW v_film
 AS
 SELECT f.film_id,title,description,release_year,f.language_id,original_language_id,rental_duration,rental_rate,length,
-       replacement_cost,rating,
+       replacement_cost,rating,replace(special_features,',','|') special_features,
+       f.last_update,l.name language,
+       concat(fc.category_id,'=',c.name) category,
+       group_concat(distinct() order by a.first_name,a.last_name separator '|') actors      
 FROM film f
 JOIN language l
 ON l.language_id = f.language_id
@@ -21,12 +24,8 @@ LEFT JOIN actor a
 ON a.actor_id = fa.actor_id
 GROUP BY f.film_id;
 
-replace(special_features, ',','|') special_features, 
-f.last_update, l.name language,
-concat(fc.category_id, '=',c.name) category, 
-group_concat(distinct concat(a.actor_id, '=',a.first_name, ' ', a.last_name) order by a.first_name, a.last_name separator '|') actors
 
--- a query to be used as is by sqoop
+-- Query was used by sqoop
 SELECT p.payment_id, p.rental_id, p.amount, p.payment_date, p.last_update, c.customer_id, 
        concat(c.first_name, ' ', c.last_name) customer_name,
        s.staff_id, concat(s.first_name, ' ', s.last_name) staff_name
