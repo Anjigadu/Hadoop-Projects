6b46e374021f6e821b36b7b0e2610d05af945297
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-06 15:23:31 -0800

    Update db_queries.sql

--- E-Commerce DW/Sqoop/db_queries.sql
+++ E-Commerce DW/Sqoop/db_queries.sql
@@ -1,110 +1,77 @@
-use adventureworks;
+USE adventureworks;
 
-create view v_salesorderheader as
-	select
-	SalesOrderID,          
-	RevisionNumber,        
-	OrderDate,             
-	DueDate,               
-	ShipDate,              
-	Status,                
-	OnlineOrderFlag,       
-	SalesOrderNumber,      
-	PurchaseOrderNumber,   
-	AccountNumber,         
-	so.CustomerID,            
-	ContactID,           
-	BillToAddressID,       
-	ShipToAddressID,       
-	ShipMethodID,          
-	CreditCardID,          
-	CreditCardApprovalCode,
-	so.CurrencyRateID,        
-	SubTotal,              
-	TaxAmt,                
-	Freight,               
-	TotalDue,              
-	Comment,              
-	so.SalesPersonID, 
-	sp.TerritoryID,
-	st.name territory,
-	st.CountryRegionCode,
-	st.group,
-	cr.FromCurrencyCode,
-	cr.toCurrencyCode,
-	cr.AverageRate,
-	cr.EndOfDayRate,
-	str.name store,
-	so.ModifiedDate         
-from salesorderheader so left join salesperson sp on sp.SalesPersonID = so.SalesPersonID
-left join salesterritory st ON st.TerritoryID = sp.TerritoryID
-left join currencyrate cr on cr.CurrencyRateID = so.CurrencyRateID 
-left join store str on str.SalesPersonID = sp.SalesPersonID;
+--(1)
+CREATE VIEW v_salesorderheader
+AS
+SELECT SalesOrderID,RevisionNumber,OrderDate,DueDate,ShipDate,Status,OnlineOrderFlag,SalesOrderNumber,PurchaseOrderNumber,   
+       AccountNumber,so.CustomerID,ContactID,BillToAddressID,ShipToAddressID,ShipMethodID,CreditCardID,
+       CreditCardApprovalCode,so.CurrencyRateID,SubTotal,TaxAmt,Freight,TotalDue,Comment,so.SalesPersonID, 
+       sp.TerritoryID,st.name territory,st.CountryRegionCode,st.group,cr.FromCurrencyCode,cr.toCurrencyCode,
+       cr.AverageRate,cr.EndOfDayRate,str.name store,so.ModifiedDate         
+FROM salesorderheader so 
+LEFT JOIN salesperson sp 
+ON sp.SalesPersonID = so.SalesPersonID
+LEFT JOIN salesterritory st 
+ON st.TerritoryID = sp.TerritoryID
+LEFT JOIN currencyrate cr 
+ON cr.CurrencyRateID = so.CurrencyRateID 
+LEFT JOIN store str 
+ON str.SalesPersonID = sp.SalesPersonID;
 
 
-create view v_salesorderdetails as
-select          
-	SalesOrderDetailID,    
-	SalesOrderID,
-	CarrierTrackingNumber, 
-	OrderQty,              
-	ProductID,             
-	UnitPrice,             
-	UnitPriceDiscount,     
-	LineTotal,       
-	sod.SpecialOfferID, 
-	so.Description,
-	so.DiscountPct,
-	so.Type,
-	so.Category
-	ModifiedDate   
-from salesorderdetail sod join specialoffer so 
+--(2)
+CREATE VIEW v_salesorderdetail
+AS
+SELECT SalesOrderDetailID,SalesOrderID,CarrierTrackingNumber,OrderQty,ProductID,UnitPrice,UnitPriceDiscount,LineTotal,       
+       sod.SpecialOfferID,so.Description,so.DiscountPct,so.Type,so.Category,ModifiedDate   
+FROM salesorderdetail sod 
+JOIN specialoffer so 
 on sod.SpecialOfferID = so.SpecialOfferID;
 
 
-create view v_product as
-select p.productId,
-	p.name,
-	productnumber,
-	makeflag,
-	finishedgoodsflag,
-	color,
-	safetystocklevel,
-	reorderpoint,
-	standardcost,
-	listprice,
-	size,
-	sizeunitmeasurecode,
-	weightunitmeasurecode,
-	weight,
-	daystomanufacture,
-	productline,
-	class,
-	style,
-	sellstartdate,
-	sellenddate,
-	discontinueddate,
-	ps.name productsubcategory,
-	pc.name productcategory,
-	pm.name producemodel,
-	pm.catalogdescription,
-	pm.instructions,
-	p.modifieddate
-from product p left join productsubcategory ps on p.productsubcategoryid = ps.productsubcategoryid
-left join productcategory pc on ps.productcategoryid = pc.productcategoryid
-left join productmodel pm on p.productmodelid = pm.productmodelid;
+--(3)
+CREATE VIEW v_product 
+AS
+SELECT p.productId, p.name, productnumber, makeflag, finishedgoodsflag, color, safetystocklevel, reorderpoint, standardcost,
+       listprice, size, sizeunitmeasurecode, weightunitmeasurecode, weight, daystomanufacture, productline, class, style,
+       sellstartdate, sellenddate, discontinueddate, ps.name productsubcategory, pc.name productcategory, 
+       pm.name producemodel, pm.catalogdescription, pm.instructions, p.modifieddate
+FROM product p 
+LEFT JOIN productsubcategory ps 
+ON p.productsubcategoryid = ps.productsubcategoryid
+LEFT JOIN productcategory pc 
+ON ps.productcategoryid = pc.productcategoryid
+LEFT JOIN productmodel pm 
+ON p.productmodelid = pm.productmodelid;
 
 
+--(4)
+CREATE VIEW v_customer 
+AS
+SELECT cus.CustomerID, cus.AccountNumber, cus.CustomerType, ind.Demographics, con.NameStyle, con.Title, con.FirstName,             
+       con.MiddleName,con.LastName, con.Suffix, con.EmailAddress, con.EmailPromotion, con.Phone, 
+       con.AdditionalContactInfo, cus.TerritoryID,t.name territoryName, t.countryregioncode, t.group, 
+       con.ModifiedDate 
+FROM customer cus
+JOIN individual ind 
+ON ind.CustomerID = cus.CustomerID 
+JOIN contact con 
+ON con.ContactID = ind.ContactID 
+JOIN salesterritory t 
+ON cus.TerritoryID = t.TerritoryID
 
-create view v_customer as 
-select cus.CustomerID,cus.AccountNumber, cus.CustomerType, ind.Demographics, con.NameStyle, con.Title, con.FirstName,             
-con.MiddleName,con.LastName, con.Suffix, con.EmailAddress, con.EmailPromotion, con.Phone, con.AdditionalContactInfo,  
-cus.TerritoryID,t.name territoryName, t.countryregioncode, t.group, con.ModifiedDate from customer cus
-join individual ind on ind.CustomerID = cus.CustomerID join contact con on con.ContactID = ind.ContactID join salesterritory t on
-cus.TerritoryID = t.TerritoryID
-union	
-select cus.CustomerID, cus.AccountNumber, cus.CustomerType, st.Demographics, con.NameStyle, con.Title, st.Name, con.MiddleName,            
-con.LastName,con.Suffix,con.EmailAddress,con.EmailPromotion, con.Phone, con.AdditionalContactInfo, cus.TerritoryID, t.name territoryName, 
-t.countryregioncode, t.group, cus.ModifiedDate from store st
-join customer cus on  st.CustomerID = cus.CustomerID join storecontact sc on  st.CustomerID = sc.CustomerID  join contact con on
-sc.ContactID = con.ContactID join salesterritory t on cus.TerritoryID = t.TerritoryID
+UNION
+
+SELECT cus.CustomerID, cus.AccountNumber, cus.CustomerType, st.Demographics, con.NameStyle, con.Title, 
+       st.Name, con.MiddleName, con.LastName, con.Suffix, con.EmailAddress, con.EmailPromotion, con.Phone, 
+       con.AdditionalContactInfo, cus.TerritoryID, t.name territoryName, t.countryregioncode, t.group, 
+       cus.ModifiedDate 
+FROM store st
+JOIN customer cus 
+ON  st.CustomerID = cus.CustomerID 
+JOIN storecontact sc 
+ON  st.CustomerID = sc.CustomerID  
+JOIN contact con 
+ON sc.ContactID = con.ContactID 
+JOIN salesterritory t 
+ON cus.TerritoryID = t.TerritoryID;
