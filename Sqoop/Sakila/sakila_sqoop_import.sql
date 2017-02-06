a4ccf3cfb263277835d61b5286795c335c6946f6
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-05 22:10:51 -0800

    Update sakila_sqoop_import.sql

--- Sqoop/Sakila/sakila_sqoop_import.sql
+++ Sqoop/Sakila/sakila_sqoop_import.sql
@@ -6,7 +6,8 @@ show schemas;
 
 CREATE VIEW v_film
 AS
-SELECT
+SELECT f.film_id,title,description,release_year,f.language_id,original_language_id,rental_duration,rental_rate,length,
+       replacement_cost,rating,
 FROM film f
 JOIN language l
 ON l.language_id = f.language_id
@@ -20,6 +21,10 @@ LEFT JOIN actor a
 ON a.actor_id = fa.actor_id
 GROUP BY f.film_id;
 
+replace(special_features, ',','|') special_features, 
+f.last_update, l.name language,
+concat(fc.category_id, '=',c.name) category, 
+group_concat(distinct concat(a.actor_id, '=',a.first_name, ' ', a.last_name) order by a.first_name, a.last_name separator '|') actors
 
 -- a query to be used as is by sqoop
 SELECT p.payment_id, p.rental_id, p.amount, p.payment_date, p.last_update, c.customer_id, 
