72ab4c9d30d7571b07e4b572e05f9dd9105da3e6
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-05 22:07:07 -0800

    Update sakila_sqoop_import.sql

--- Sqoop/Sakila/sakila_sqoop_import.sql
+++ Sqoop/Sakila/sakila_sqoop_import.sql
@@ -1,23 +1,34 @@
-[MYSQL]
+-- [MYSQL]
 show databases;
 show schemas;
 
-unzip world.zip
+-- Query to create a view for film display. Later used as a table by hive
 
+CREATE VIEW v_film
+AS
+SELECT
+FROM film f
+JOIN language l
+ON l.language_id = f.language_id
+JOIN film_category fc
+ON fc.film_id = f.film_id
+JOIN category c
+ON c.category_id = fc.category_id
+LEFT JOIN film_actor fa 
+ON fa.film_id = f.film_id
+LEFT JOIN actor a
+ON a.actor_id = fa.actor_id
+GROUP BY f.film_id;
 
---query to create a view for film display. Later used as a table by hive
-create view v_film as
-select f.film_id, title, description, release_year, f.language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, replace(special_features, ',','|') special_features, f.last_update, l.name language,
-concat(fc.category_id, '=',c.name) category, group_concat(distinct concat(a.actor_id, '=',a.first_name, ' ', a.last_name) order by a.first_name, a.last_name separator '|') actors
-from film f join language l on l.language_id = f.language_id
-join film_category fc on fc.film_id = f.film_id
-join category c on c.category_id = fc.category_id
-left join film_actor fa on fa.film_id = f.film_id
-  left join actor a on a.actor_id = fa.actor_id
-group by f.film_id
 
 -- a query to be used as is by sqoop
-SELECT p.payment_id, p.rental_id, p.amount, p.payment_date, p.last_update, c.customer_id, concat(c.first_name, ' ', c.last_name) customer_name,
-s.staff_id, concat(s.first_name, ' ', s.last_name) staff_name
- FROM payment p  join customer c on c.customer_id = p.customer_id  join staff s on s.staff_id = p.staff_id
-join rental r on r.rental_id = p.rental_id
+SELECT p.payment_id, p.rental_id, p.amount, p.payment_date, p.last_update, c.customer_id, 
+       concat(c.first_name, ' ', c.last_name) customer_name,
+       s.staff_id, concat(s.first_name, ' ', s.last_name) staff_name
+FROM payment p  
+join customer c 
+on c.customer_id = p.customer_id  
+join staff s 
+on s.staff_id = p.staff_id
+join rental r 
+on r.rental_id = p.rental_id;
