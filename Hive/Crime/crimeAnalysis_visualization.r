a55c5b350671c4dd269cda8425bc9493a9f9b332
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-07 23:26:50 -0800

    Update crimeAnalysis_visualization.r

--- Hive/Crime/crimeAnalysis_visualization.r
+++ Hive/Crime/crimeAnalysis_visualization.r
@@ -1,3 +1,4 @@
+# Impala ODBC Driver + R
 conn <- odbcConnect("Impala")
 crime <- sqlQuery(conn,
                   "select substring(reportdatetime,1,4) as year, substring(reportdatetime,6,2) as month, 
@@ -5,7 +6,11 @@ crime <- sqlQuery(conn,
                    from crime_incident 
                    group by substring(reportdatetime,1,4),substring(reportdatetime,6,2),method,offense 
                    order by 1,2")
+# ggplot2 packages 
 install.packages("ggplot2")
 library(ggplot2)
+
+# (1) Bar Chart
 pl <- ggplot(crime,aes(y=frequency, x=year,fill=method))
 pl + geom_bar(stat="identity")
+
