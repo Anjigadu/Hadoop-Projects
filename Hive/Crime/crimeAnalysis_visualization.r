dfeb3f817449c3e7b8f27712e271a1fc359452cc
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-07 23:25:21 -0800

    Create crimeAnalysis_visualization.r

---
+++ Hive/Crime/crimeAnalysis_visualization.r
@@ -0,0 +1,11 @@
+conn <- odbcConnect("Impala")
+crime <- sqlQuery(conn,
+                  "select substring(reportdatetime,1,4) as year, substring(reportdatetime,6,2) as month, 
+                          method,offense, count(1) as frequency 
+                   from crime_incident 
+                   group by substring(reportdatetime,1,4),substring(reportdatetime,6,2),method,offense 
+                   order by 1,2")
+install.packages("ggplot2")
+library(ggplot2)
+pl <- ggplot(crime,aes(y=frequency, x=year,fill=method))
+pl + geom_bar(stat="identity")
