4398acfa9740090befcc6898ace203cc279e4629
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-08 01:51:51 -0800

    Update crimeAnalysis_visualization.r

--- Hive/Crime/crimeAnalysis_visualization.r
+++ Hive/Crime/crimeAnalysis_visualization.r
@@ -1,5 +1,6 @@
-# Impala ODBC Driver + R
+## Impala ODBC Driver + R
 conn <- odbcConnect("Impala")
+## Query 1. Summary between year 2010 and year 2016
 crime <- sqlQuery(conn,
                   "select substring(reportdatetime,1,4) as year, substring(reportdatetime,6,2) as month, 
                           method,offense, count(1) as frequency 
@@ -10,7 +11,22 @@ crime <- sqlQuery(conn,
 install.packages("ggplot2")
 library(ggplot2)
 
-# (1) Bar Chart
+# Bar Chart
 pl <- ggplot(crime,aes(y=frequency, x=year,fill=method))
 pl + geom_bar(stat="identity")
 
+## Query 2. 
+offense <- sqlQuery(conn,"
+select substring(reportdatetime,1,4) year,offense,count(offense) frequency 
+from crime_incident 
+where substring(reportdatetime,1,4) between '2010' and '2016'
+group by substring(reportdatetime,1,4),offense")
+
+ggplot(offense, aes(x = year,fill=offense)) 
++ geom_line(aes(y = frequency), colour="grey") 
++ ylab(label="Number of Event Occurred") 
++ xlab("year")
++ geom_point()
+                     
+                    
+    values=c(gg_color_hue(length(unique(dfr$group))),
