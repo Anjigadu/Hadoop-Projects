125b0c6fe924a984847fca9a6f6c93e962b9cd87
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-08 20:33:44 -0800

    Update customer_demographic_processor.scala

--- E-Commerce DW/Spark/customer_demographic_processor.scala
+++ E-Commerce DW/Spark/customer_demographic_processor.scala
@@ -1,9 +1,39 @@
+$ spark-shell
+
 import scala.xml.XML
 
 val customerRDD = sqlContext.read.parquet("/user/cloudera/bigretail/output/stores/sqoop/customers")
 val projDF = customerRDD.select("CustomerID","Demographics")
 
-case class CustomerDemo(CustomerID: Int, TotalPurchaseYTD: String, DateFirstPurchase : String, BirthDate : String, MaritalStatus : String, YearlyIncome : String, Gender : String, TotalChildren : String, NumberChildrenAtHome : String, Education : String, Occupation : String, HomeOwnerFlag : String, NumberCarsOwned : String, CommuteDistance : String) extends java.io.Serializable
+# projDF.first
+res1: org.apache.spark.sql.Row = [11000,<IndividualSurvey xmlns="http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey">
+<TotalPurchaseYTD>8248.99</TotalPurchaseYTD>
+<DateFirstPurchase>2001-07-22Z</DateFirstPurchase>
+<BirthDate>1966-04-08Z</BirthDate>
+<MaritalStatus>M</MaritalStatus>
+				  <YearlyIncome>75001-100000</YearlyIncome>
+				  <Gender>M</Gender>
+				  <TotalChildren>2</TotalChildren>
+				  <NumberChildrenAtHome>0</NumberChildrenAtHome>
+				  <Education>Bachelors </Education>
+				  <Occupation>Professional</Occupation>
+				  <HomeOwnerFlag>1</HomeOwnerFlag>
+				  <NumberCarsOwned>0</NumberCarsOwned>
+				  <CommuteDistance>1-2 Miles</CommuteDistance>
+				  </IndividualSurvey>]
+
+
+
+
+
+
+
+
+case class CustomerDemo(CustomerID: Int, TotalPurchaseYTD: String, DateFirstPurchase : String, 
+			BirthDate : String, MaritalStatus : String, YearlyIncome : String, Gender : String, 
+			TotalChildren : String, NumberChildrenAtHome : String, Education : String, 
+			Occupation : String, HomeOwnerFlag : String, NumberCarsOwned : String, 
+			CommuteDistance : String) extends java.io.Serializable
 
 def createCustDemo(custId: Int, demo:String) : CustomerDemo = {
 	val node = XML.loadString(demo)
