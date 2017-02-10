20efeba7ba4e62ab7bcacec4a8649f0ea74e7e59
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-02-09 16:41:48 -0800

    Update customer_demographic_processor.scala

--- E-Commerce DW/Spark/customer_demographic_processor.scala
+++ E-Commerce DW/Spark/customer_demographic_processor.scala
@@ -3,37 +3,44 @@ $ spark-shell
 import scala.xml.XML
 
 val customerRDD = sqlContext.read.parquet("/user/cloudera/bigretail/output/stores/sqoop/customers")
+
+# customerRDD: org.apache.spark.sql.DataFrame = [CustomerID: int, AccountNumber: string, 
+#     					         CustomerType: string, Demographics: string, 
+#					         NameStyle: boolean, Title: string, FirstName: string, 
+#					         MiddleName: string, LastName: string, Suffix: string, 
+#					         EmailAddress: string, EmailPromotion: int, Phone: string, 
+#					         AdditionalContactInfo: string, TerritoryID: int, territoryName: string, 
+#					         countryregioncode: string, group: string, ModifiedDate: bigint]
+#
+
 val projDF = customerRDD.select("CustomerID","Demographics")
 
 # projDF.first
-res1: org.apache.spark.sql.Row = [11000,<IndividualSurvey xmlns="http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey">
-<TotalPurchaseYTD>8248.99</TotalPurchaseYTD>
-<DateFirstPurchase>2001-07-22Z</DateFirstPurchase>
-<BirthDate>1966-04-08Z</BirthDate>
-<MaritalStatus>M</MaritalStatus>
-				  <YearlyIncome>75001-100000</YearlyIncome>
-				  <Gender>M</Gender>
-				  <TotalChildren>2</TotalChildren>
-				  <NumberChildrenAtHome>0</NumberChildrenAtHome>
-				  <Education>Bachelors </Education>
-				  <Occupation>Professional</Occupation>
-				  <HomeOwnerFlag>1</HomeOwnerFlag>
-				  <NumberCarsOwned>0</NumberCarsOwned>
-				  <CommuteDistance>1-2 Miles</CommuteDistance>
-				  </IndividualSurvey>]
-
-
-
-
-
-
-
+# res1: org.apache.spark.sql.Row = [11000,<IndividualSurvey xmlns="http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey">
+# <TotalPurchaseYTD>8248.99</TotalPurchaseYTD>
+# <DateFirstPurchase>2001-07-22Z</DateFirstPurchase>
+# <BirthDate>1966-04-08Z</BirthDate>
+# <MaritalStatus>M</MaritalStatus>
+# <YearlyIncome>75001-100000</YearlyIncome>
+# <Gender>M</Gender>
+# <TotalChildren>2</TotalChildren>
+# <NumberChildrenAtHome>0</NumberChildrenAtHome>
+# <Education>Bachelors </Education>
+# <Occupation>Professional</Occupation>
+# <HomeOwnerFlag>1</HomeOwnerFlag>
+# <NumberCarsOwned>0</NumberCarsOwned>
+# <CommuteDistance>1-2 Miles</CommuteDistance>
+# </IndividualSurvey>]
 
 case class CustomerDemo(CustomerID: Int, TotalPurchaseYTD: String, DateFirstPurchase : String, 
 			BirthDate : String, MaritalStatus : String, YearlyIncome : String, Gender : String, 
 			TotalChildren : String, NumberChildrenAtHome : String, Education : String, 
 			Occupation : String, HomeOwnerFlag : String, NumberCarsOwned : String, 
 			CommuteDistance : String) extends java.io.Serializable
+
+# defined class CustomerDemo
+
+def createCustomerDemo(custId: Int,
 
 def createCustDemo(custId: Int, demo:String) : CustomerDemo = {
 	val node = XML.loadString(demo)
